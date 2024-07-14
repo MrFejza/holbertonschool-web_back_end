364a6d6068a36e02c3ea4754cb20a5762b599fed
MrFejza <fejzaergis@gmail.com>
2024-07-14 20:07:06 +0200

    task 3

---
+++ ES6_promise/dist/3-main.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _all = _interopRequireDefault(require("./3-all"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+(0, _all["default"])();
