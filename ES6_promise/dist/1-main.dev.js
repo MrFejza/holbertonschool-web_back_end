6ac39952728b46e86a697220e465918c36db1d55
MrFejza <fejzaergis@gmail.com>
2024-07-16 18:16:04 +0200

    task 1

---
+++ ES6_promise/dist/1-main.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _promise = _interopRequireDefault(require("./1-promise"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log((0, _promise["default"])(true));
+console.log((0, _promise["default"])(false));
---
+++ ES6_promise/dist/1-promise.dev.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = getFullResponseFromAPI;
+
+function getFullResponseFromAPI(success) {
+  return new Promise(function (resolve, reject) {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
