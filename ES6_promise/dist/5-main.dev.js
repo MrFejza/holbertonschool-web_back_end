c0f328dc78279c190165cbd7bcda176b1d08e7ac
MrFejza <fejzaergis@gmail.com>
2024-07-16 18:59:01 +0200

    task 5

---
+++ ES6_promise/dist/5-main.dev.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _photoReject = _interopRequireDefault(require("./5-photo-reject"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+console.log((0, _photoReject["default"])('guillaume.jpg'));
---
+++ ES6_promise/dist/5-photo-reject.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = uploadPhoto;
+
+function uploadPhoto(fileName) {
+  return Promise.reject(new Error("".concat(fileName, " cannot be processed")));
+}
