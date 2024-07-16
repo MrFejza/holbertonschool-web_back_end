9289e9d94a2f9ef38580a79e424020492ba51f64
MrFejza <fejzaergis@gmail.com>
2024-07-16 18:07:33 +0200

    restart

--- ES6_promise/dist/2-main.dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _then = _interopRequireDefault(require("./2-then"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-var promise = Promise.resolve();
-(0, _then["default"])(promise);
--- ES6_promise/dist/3-main.dev.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _all = _interopRequireDefault(require("./3-all"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-(0, _all["default"])();
