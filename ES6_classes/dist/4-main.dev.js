6eb9644c3ad0afe4854312d78327836f6f1437eb
MrFejza <fejzaergis@gmail.com>
2024-07-22 17:46:43 +0200

    task 4

--- ES6_classes/dist/4-main.dev.js
+++ ES6_classes/dist/4-main.dev.js
@@ -1,11 +1,11 @@
 "use strict";
 
-var _pricing = _interopRequireDefault(require("./4-pricing"));
+var _pricing = _interopRequireDefault(require("./4-pricing.js"));
 
-var _currency = _interopRequireDefault(require("./3-currency"));
+var _currency = _interopRequireDefault(require("./3-currency.js"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-var p = new _pricing["default"](100, new _currency["default"]('EUR', 'Euro'));
+var p = new _pricing["default"](100, new _currency["default"]("EUR", "Euro"));
 console.log(p);
 console.log(p.displayFullPrice());
