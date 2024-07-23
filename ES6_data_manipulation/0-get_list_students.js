8b123e5754f102abad262fb67ce35614a204b23d
MrFejza <fejzaergis@gmail.com>
2024-07-23 17:53:41 +0200

    task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,21 @@
-export default function getListStudents(){
-let students =[
-  {
-    firstName:"Guillaume",
-    id: 1,
-    location: "San Francisco"
-  },
-  {
-    firstName:"James",
-    id: 2,
-    location: "Columbia "
-  },
-  {
-    firstName:"Serena",
-    id: 5,
-    location: "San Francisco"
-  }
-];
+export default function getListStudents() {
+  const students = [
+    {
+      firstName: "Guillaume",
+      id: 1,
+      location: "San Francisco"
+    },
+    {
+      firstName: "James",
+      id: 2,
+      location: "Columbia"
+    },
+    {
+      firstName: "Serena",
+      id: 5,
+      location: "San Francisco"
+    }
+  ];
 
-return students;
+  return students;
 }
