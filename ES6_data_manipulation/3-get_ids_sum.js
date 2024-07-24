93dc7fc66ab92a475cc650fd3c615c62ff9a82dc
MrFejza <fejzaergis@gmail.com>
2024-07-24 16:53:43 +0200

    task 3

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,6 @@
 export default function getStudentIdsSum(listStudents) {
  const interValue = 0;
-  const sum = listStudents.reduce ((acumulation, Ids) => {
-    acumulation += Ids.id, interValue;
-  })
+  const sum = listStudents.reduce ((acumulation, Ids) => acumulation += Ids.id, interValue);
 
   return sum;
 }
