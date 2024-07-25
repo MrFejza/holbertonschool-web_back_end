8ffd3444f569639021f71334df186b8a6f6651ba
MrFejza <fejzaergis@gmail.com>
2024-07-25 18:17:57 +0200

    task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,10 +3,10 @@ export default function updateStudentGradeByCity(listOfStudent, city, newGrade) 
   const addGrade = filterStudent.map((student) => {
     const grade = newGrade.find((grade) => grade.studentId === student.id);
     if (grade) {
-
+      // eslint-disable-next-line no-param-reassign
       student.grade = grade.grade;
     } else {
-
+      // eslint-disable-next-line no-param-reassign
       student.grade = 'N/A';
     }
     return student;
