da5c95860d69f8bca5c1fb56a22e68d0c8130c29
MrFejza <fejzaergis@gmail.com>
2024-07-25 16:36:32 +0200

    task 4

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+export default function updateStudentGradeByCity(students, city, newGrade) {
+  const filterStudent = students.filter((student) => student.location === city );
+  const addGrade = filterStudent.map((student) => {
+    const grade = newGrade.find((grade) => grade.studentId === student.id);
+    if(grade){
+      student.grade =grade.grade;
+    }else{
+      student.grade = `N/A`;
+    }
+    return student;
+  });
+  return addGrade;
+}
