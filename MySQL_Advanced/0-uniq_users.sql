2ee67825edaa1d11c1e8fd7864c3b87d3958b802
MrFejza <fejzaergis@gmail.com>
2024-08-21 18:25:19 +0200

    added a comment

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,4 @@
+--this script should not fail if the table already exists
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
