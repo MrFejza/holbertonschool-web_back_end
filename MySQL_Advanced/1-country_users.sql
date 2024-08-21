1b01a0b9ba2baf206413dab6a4896cac98b14626
MrFejza <fejzaergis@gmail.com>
2024-08-21 18:40:02 +0200

    task 1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,9 @@
 -- Make a table
 -- If table exist, you should not fail
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US','CO','TN') NOT NULL DEFAULT 'US',
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
     PRIMARY KEY(id)
 );
