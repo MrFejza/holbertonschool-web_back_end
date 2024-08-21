8855793382cdcf083c1677667c88ae03425a4c5b
MrFejza <fejzaergis@gmail.com>
2024-08-21 18:34:10 +0200

    task 1

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Make a table
+-- If table exist, you should not fail
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US','CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY(id)
+);
