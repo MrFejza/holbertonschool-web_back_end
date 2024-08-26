fbad61b4a5014b490f02348770859a8d47e5eca7
MrFejza <fejzaergis@gmail.com>
2024-08-26 17:49:53 +0200

    taks 6

--- MySQL_Advanced/6-bonus.sql
+++ MySQL_Advanced/6-bonus.sql
@@ -1,31 +1,25 @@
 DELIMITER $$
 
 CREATE PROCEDURE AddBonus(
-IN user_id INTEGER,
-IN project_name VARCHAR(255),
-IN score INTIGER
+    IN user_id INTEGER,
+    IN project_name VARCHAR(255),
+    IN score INTEGER
 )
-
 BEGIN
-INSERT INTO project(name)
-SELECT project_name
-FROM DUAL 
-WHERE NOT EXISTS (
-    SELECT *
-    FROM project
-    WHERE name = project_name
-    LIMIT 1
-)
 
-INSERT INTO corrections(user_id, project_id, score)
-VALUES(
-    user_id,
-    (SELECT id
-    FROM project
-    WHERE name = project_name),
-    score
-)
+    IF NOT EXISTS (
+        SELECT 1 FROM projects WHERE name = project_name
+    ) THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+
+
+    INSERT INTO corrections(user_id, project_id, score)
+    VALUES(
+        user_id,
+        (SELECT id FROM projects WHERE name = project_name),
+        score
+    );
 END $$
-
 
 DELIMITER ;
